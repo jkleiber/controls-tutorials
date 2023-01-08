9fb188ec17c414065034b846be0f64342c4b9653
Justin Kleiber <jkleiber8@gmail.com>
2023-01-08 17:22:44 -0500

    Removes absolute link in JS

--- app/static/js/html_loader.js
+++ app/static/js/html_loader.js
@@ -2,7 +2,7 @@
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
-      var file = '/static/components/' + $(this).data('include') + '.html' + '?_=' + Math.round(Math.random()*10000)
+      var file = 'static/components/' + $(this).data('include') + '.html' + '?_=' + Math.round(Math.random()*10000)
       $(this).load(file)
     })
   })
