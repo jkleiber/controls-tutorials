c3c562dacbeb59d735ea742c10ae0f162f13367f
Justin Kleiber <jkleiber8@gmail.com>
2023-01-08 18:32:35 -0500

    Gets the chart to show up and not be terrible

--- app/static/js/pid_control_demo.js
+++ app/static/js/pid_control_demo.js
@@ -2,3 +2,17 @@
 /*
  * Make sure to include: <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script> before this script 
  */
+window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/eligrey-classlist-js-polyfill@1.2.20171210/classList.min.js"><\/script>'
+        )
+      window.Promise ||
+        document.write(
+          '<script src="https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn"><\/script>'
+        )
+
+
